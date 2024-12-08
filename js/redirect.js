b28528aaf5a7e396603f9cc090e737c647a34a3e
AcronicalYT <ace36501@hotmail.com>
2024-12-08 21:18:13 +0000

    home + contact

--- js/redirect.js
+++
@@ -1,8 +0,0 @@
-function redirect() {
-    const current = window.location.href;
-    const exclude = current.indexOf('?');
-    if (!exclude) window.location = '/';
-    window.location = current.substring(exclude + 1);
-}
-
-setTimeout(redirect, 5000);
