5cc4494de84a990d41ae7740aa2558616efad107
AcronicalYT <ace36501@hotmail.com>
2024-12-08 23:18:56 +0000

    progress

--- js/navMenu.js
+++ js/navMenu.js
@@ -1,21 +1,6 @@
-const navUl = document.querySelector('.mob-nav-ul');
-const menuBtn = document.querySelector('.nav-open');
-const closeBtn = document.querySelector('.nav-close');
-const body = document.querySelector('body');
+const menuToggle = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
 
-window.onresize = async function windowResize() {
-    if(window.innerWidth >= 768 && navUl.classList.contains('show')) toggleNav();
-}
-
-menuBtn.addEventListener('click', () => {
-    toggleNav();
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('open');
 });
-
-closeBtn.addEventListener('click', () => {
-    toggleNav();
-});
-
-function toggleNav() {
-    navUl.classList.toggle('show'); 
-    menuBtn.classList.toggle('hide');
-}
