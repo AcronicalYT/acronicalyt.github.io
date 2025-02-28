335124ad441ef8ba4c1e812e6a53047893080978
Acronical <ace36501@hotmail.com>
2025-02-28 10:20:51 +0000

    e2

--- js/uptime.js
+++ js/uptime.js
@@ -52,7 +52,7 @@ window.onload = async () => {
     const services = await fetchAllServices();
     console.log(services);
     for (const [key, service] of Object.entries(services)) {
-        if (!service.isActive) continue;
+        if (!service.online) continue;
         const serviceElement = document.createElement('li');
         serviceElement.classList.add('dropdown-item');
         serviceElement.textContent = service.name;
